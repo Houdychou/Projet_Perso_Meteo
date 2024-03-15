fd21a2dcfe846d30008ed08cfb3af4473436b2d9
Houdychou <hchouchouh@edenschool.fr>
2024-03-15 11:51:00 +0100

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
 const apiKey = 'cf79a8b955dcf72f938b8fcedead8188';
-fetch('https://api.openweathermap.org/data/2.5/weather?q={}&appid={apiKey}');
+const city = 'Marrakech';
+function nameOfDays() {
+    let date = new Date();
+    let numeroJour = date.getDay();
+    let joursSemaine = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    let nomJour = joursSemaine[numeroJour];
+    return nomJour;
+}
+function meteoFetch() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        const location = document.querySelector('.location');
+        const rain = document.querySelector('.rain');
+        const temp = document.querySelector('.temp');
+        const humidity = document.querySelector('.humidity');
+        const wind = document.querySelector('.wind');
+        const tempCelsius = data.main.temp - 273.15;
+        console.log(nameOfDays())
+        location.textContent = data.name + ', ' + data.sys.country;
+        temp.textContent =  + tempCelsius.toFixed(0) + '°C';
+        humidity.textContent = 'humidity : ' + data.main.humidity + '%';
+        wind.textContent = 'Wind : ' + data.wind.speed + 'km/h';
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+}
+meteoFetch();
