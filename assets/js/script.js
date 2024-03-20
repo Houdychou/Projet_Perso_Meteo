8bfa34fb0b20897d4fb8a16e8854dc9140f0fa25
Houdychou <hchouchouh@edenschool.fr>
2024-03-20 11:18:43 +0100

    mess erreur

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,13 @@
 const apiKey = 'cf79a8b955dcf72f938b8fcedead8188';
-const city = 'Marrakech';
 const cityInput = document.querySelector('input');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
-const icon = document.querySelector('.fa-magnifying-glass');
 const headerImg = document.querySelector('header img');
 const date = document.querySelector('.date');
+const erreur = document.querySelector('.error');
 const tempMax = document.querySelectorAll('.temp-max');
 const tempMin = document.querySelectorAll('.temp-min');
-const footerImg = document.querySelectorAll('footer img');
 const locationElement = document.querySelector('.location'); 
-
-const ImageMeteo = [
-    {
-        "id": 1,
-        "image": "assets/img/neige.png"
-    },
-    {
-        "id": 2,
-        "image": "assets/img/orage.png"
-    },
-    {
-        "id": 3,
-        "image": "assets/img/soleil-nuage.png"
-    },
-    {
-        "id": 4,
-        "image": "assets/img/soleil.png"
-    },
-    {
-        "id": 5,
-        "image": "assets/img/pluie.png"
-    }  
-]
 
 function nameOfDays() {
     let date = new Date();
@@ -45,6 +20,9 @@ function nameOfDays() {
 function meteoFetch(city) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
     .then(function(res) {
+        if (!res.ok) {
+            throw new Error('City not found');
+        }
         return res.json();
     })
     .then(function(data) {
@@ -56,15 +34,20 @@ function meteoFetch(city) {
         const tempCelsius = data.main.temp - 273.15;
         date.textContent = nameOfDays();
         location.textContent = data.name + ', ' + data.sys.country;
-        temp.textContent =  + tempCelsius.toFixed(0) + '°C';
+        temp.textContent = tempCelsius.toFixed(0) + '°C';
         humidity.textContent = 'humidity : ' + data.main.humidity + '%';
         wind.textContent = 'Wind : ' + data.wind.speed + 'km/h';
     })
     .catch(function(error) {
         console.log(error);
+        erreur.classList.remove('hidden');
+        main.classList.add('hidden');
+        footer.classList.add('hidden');
+        date.classList.add('hidden');
+        headerImg.classList.add('hidden')
+        locationElement.classList.add('hidden');
     })
 }
-meteoFetch();
 
 function meteoForecast(city) {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey)
@@ -94,13 +77,15 @@ cityInput.addEventListener('input', function (event) {
         date.classList.add('hidden');
         headerImg.classList.add('hidden');
         locationElement.classList.add('hidden');
+    } else {
+        erreur.classList.add('hidden');
     }
 });
 
 cityInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        const city = cityInput.value;
-        if (city.trim() !== "") {
+        const city = cityInput.value.trim(); 
+        if (city !== "") {
             main.classList.remove('hidden');
             footer.classList.remove('hidden');
             date.classList.remove('hidden');
@@ -108,6 +93,14 @@ cityInput.addEventListener('keydown', function (event) {
             locationElement.classList.remove('hidden');
             meteoFetch(city);
             meteoForecast(city);
+            erreur.classList.add('hidden');
+        } else {
+            main.classList.add('hidden');
+            footer.classList.add('hidden');
+            date.classList.add('hidden');
+            headerImg.classList.add('hidden');
+            locationElement.classList.add('hidden');
+            erreur.classList.remove('hidden');
         }
     }
 });
